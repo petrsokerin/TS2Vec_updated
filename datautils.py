6a9be9b2599e9bae4b6d962a73d3f22ce18d8020
petrsokerin <petya4343@mail.ru>
2024-07-28 17:42:51 +0300

    debug datautils

--- datautils.py
+++ datautils.py
@@ -130,8 +130,8 @@ def _get_time_features(dt):
         dt.day.to_numpy(),
         dt.dayofyear.to_numpy(),
         dt.month.to_numpy(),
-        dt.weekofyear.to_numpy(),
-    ], axis=1).astype(np.float)
+        dt.isocalendar().week.to_numpy(),
+    ], axis=1).astype(np.float64)
 
 
 def load_forecast_csv(name, univar=False):
